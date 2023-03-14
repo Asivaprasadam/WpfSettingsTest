64f34d2ab3df390599b38427f2156b9303e0d0f2
Arjun Sivaprasadam <asivaprasadam@chargerind.com>
2023-03-14 14:33:28 -0600

    Model 1 for test!

--- WpfSettingsTest/MainWindow.xaml.cs
+++ WpfSettingsTest/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Drawing.Text;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Media;
 
 namespace WpfSettingsTest;
 /// <summary>
@@ -6,9 +10,25 @@ namespace WpfSettingsTest;
 /// </summary>
 public partial class MainWindow : Window
 {
+    public static List<string> FontsList { get; set; } = GetFontNames();
+
     public MainWindow()
     {
         InitializeComponent();
+        this.DataContext = this;
+    }
+
+    public static List<string> GetFontNames()
+    {
+        List<string> fontNames = new List<string>();
+        using (InstalledFontCollection installedFonts = new InstalledFontCollection())
+        {
+            foreach (var fontFamily in installedFonts.Families)
+            {
+                fontNames.Add(fontFamily.Name);
+            }
+        }
+        return fontNames;
     }
 }
 
