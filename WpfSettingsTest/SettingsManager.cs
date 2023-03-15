90e2363782d3b35f542d3296fd468ee1a3cc0ee8
Arjun Sivaprasadam <asivaprasadam@chargerind.com>
2023-03-15 17:41:21 -0600

    Refactoring!

--- WpfSettingsTest/SettingsManager.cs
+++ WpfSettingsTest/SettingsManager.cs
@@ -117,8 +117,3 @@ public class SettingsManager
         return properties;
     }
 }
-
-public static class TypeExtensions
-{
-    public static object? GetDefaultValue(this Type type) => type.IsValueType ? Activator.CreateInstance(type) : null;
-}
